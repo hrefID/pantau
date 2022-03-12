64255fab729885534bd323bf826b9069e38e322e
elianrichard <elianrichards@gmail.com>
2022-03-12 12:18:37 +0700

    feature hotfix

--- js/dashboard.js
+++ js/dashboard.js
@@ -353,7 +353,7 @@ document.querySelectorAll(".person-name").forEach((a) => {
       document
         .querySelector(".toolbar-toggle")
         .querySelector("path").attributes.d.value =
-        "M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z";
+        "M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z";
     }
   });
 });
